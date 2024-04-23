f7833c1cd8b9a6d2704355425236044a9cf51398
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-04-23 20:05:10 +0500

    full done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { Provider } from "react-redux";
 
 // pages
 import SignIn from "./pages/SignIn";
@@ -6,6 +7,7 @@ import ErrorPage from "./pages/ErrorPage";
 import Profil from "./pages/Profil";
 import CategoriesPage from "./pages/CategoriesPage";
 import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
 
 // layouts
 import RootLayouts from "./layouts/RootLayouts";
@@ -13,6 +15,7 @@ import RootLayouts from "./layouts/RootLayouts";
 // blur css
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { useEffect } from "react";
+import { store } from "./redux/store";
 
 function App() {
   const navigate = useNavigate();
@@ -29,15 +32,18 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path=":restaurantId" element={<RootLayouts />}>
-          <Route index element={<Home />} />
-          <Route path=":categoryId" element={<CategoriesPage />} />
-          <Route path="profile" element={<Profil />} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path=":restaurantId" element={<RootLayouts />}>
+            <Route index element={<Home />} />
+            <Route path=":categoryId" element={<CategoriesPage />} />
+            <Route path="profile" element={<Profil />} />
+            <Route path="search" element={<SearchPage />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
